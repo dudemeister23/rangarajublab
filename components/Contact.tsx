1bf6b321f1634fd057a60b674ab157dbc3eb0840
dudemeister23 <87572751+dudemeister23@users.noreply.github.com>
2026-01-07 10:49:11 -0500

    docs: Update recruitment text in Contact component from 'motivated' to 'talented'.

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -81,7 +81,7 @@ const Contact: React.FC = () => {
                 Join Our Team
                 <i className="fa-solid fa-envelope"></i>
               </a>
-              <p className="text-sm text-neuro-100 leading-relaxed">We are always looking for motivated Postdocs and PhD students interested in neuroenergetics.</p>
+              <p className="text-sm text-neuro-100 leading-relaxed">We are always looking for talented Postdocs and PhD students interested in neuroenergetics.</p>
             </div>
           </div>
 
